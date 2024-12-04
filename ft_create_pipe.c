5b44db4fb6034fdc9ce51b4935edcfb9150e8521
Emma Blanchard <eblancha@bess-f4r6s2.clusters.42paris.fr>
2024-12-04 17:48:56 +0100

    small correct for wrong commad

--- ft_create_pipe.c
+++ ft_create_pipe.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_create_pipe.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eblancha <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: eblancha <eblancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/04 17:42:09 by eblancha          #+#    #+#             */
-/*   Updated: 2024/12/04 17:44:38 by eblancha         ###   ########.fr       */
+/*   Updated: 2024/12/04 17:47:37 by eblancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,8 @@ void	child_process_1(t_pipe_args *args, int pipe_fd[2])
 
 	args->path_cmd = args->path_cmd1;
 	args->cmd = args->cmd1;
+	if (!args->path_cmd)
+		perror_exit("Error: wrong command\n");
 	infile = open(args->file1, O_RDONLY);
 	if (infile == -1)
 		perror_exit("Error: cannot open file1\n");
@@ -43,6 +45,8 @@ void	child_process_2(t_pipe_args *args, int pipe_fd[2])
 
 	args->path_cmd = args->path_cmd2;
 	args->cmd = args->cmd2;
+	if (!args->path_cmd)
+		perror_exit("Error: wrong command\n");
 	outfile = open(args->file2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (outfile == -1)
 		perror_exit("Error: cannot write in file2\n");
