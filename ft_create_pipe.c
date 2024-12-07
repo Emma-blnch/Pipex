7388405ea4608537640f2621d1b713bc6e41bab4
Emma Blanchard <emma.blnch4@gmail.com>
2024-12-07 14:22:42 +0100

    Update ft_create_pipe.c

--- ft_create_pipe.c
+++ ft_create_pipe.c
@@ -31,12 +31,7 @@ void	child_process_1(t_pipe_args *args, int pipe_fd[2])
 	args->path_cmd = args->path_cmd1;
 	args->cmd = args->cmd1;
 	if (!args->path_cmd)
-{
-    write(STDERR_FILENO, args->cmd[0], ft_strlen(args->cmd[0]));
-    write(STDERR_FILENO, ": command not found\n", 20);
-    exit(127);
-}
-	//perror_exit("Error: wrong command");
+		perror_exit("Error: command not found");
 	infile = open(args->file1, O_RDONLY);
 	if (infile == -1)
 		perror_exit("Error: cannot open file1");
@@ -50,13 +45,8 @@ void	child_process_2(t_pipe_args *args, int pipe_fd[2])
 
 	args->path_cmd = args->path_cmd2;
 	args->cmd = args->cmd2;
-if (!args->path_cmd)
-{
-    write(STDERR_FILENO, args->cmd[0], ft_strlen(args->cmd[0]));
-    write(STDERR_FILENO, ": command not found\n", 20);
-    exit(127);
-}
-	//perror_exit("Error: wrong command");
+	if (!args->path_cmd)
+		perror_exit("Error: command not found");
 	outfile = open(args->file2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (outfile == -1)
 	{
