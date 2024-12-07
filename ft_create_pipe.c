06423d34b3fc6a72fd09c3adf2e666df92de70b5
Emma Blanchard <emma.blnch4@gmail.com>
2024-12-07 13:53:01 +0100

    Update ft_create_pipe.c

--- ft_create_pipe.c
+++ ft_create_pipe.c
@@ -21,7 +21,10 @@ void	execute_command(int infile, int outfile, t_pipe_args *args)
 	close(infile);
 	close(outfile);
 	execve(args->path_cmd, args->cmd, args->envp);
-	perror_exit("Error: execve failed\n");
+	if (errno == ENOENT) // Commande introuvable
+        	exit(127);
+	else
+		perror_exit("Error: execve failed\n"); // Autres erreurs système
 }
 
 void	child_process_1(t_pipe_args *args, int pipe_fd[2])
