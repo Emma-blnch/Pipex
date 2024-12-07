b83c35576fca5b7ae10eefba9cd361c2fa1b1879
Emma Blanchard <emma.blnch4@gmail.com>
2024-12-07 14:15:02 +0100

    Update ft_create_pipe.c

--- ft_create_pipe.c
+++ ft_create_pipe.c
@@ -15,13 +15,13 @@
 void	execute_command(int infile, int outfile, t_pipe_args *args)
 {
 	if (dup2(infile, STDIN_FILENO) == -1)
-		perror_exit("Error: dup2 input_fd failed\n");
+		perror_exit("dup2 failed");
 	if (dup2(outfile, STDOUT_FILENO) == -1)
 		perror_exit("Error: dup2 output_fd failed\n");
 	close(infile);
 	close(outfile);
 	execve(args->path_cmd, args->cmd, args->envp);
-	perror_exit("Error: execve failed\n");
+	perror_exit("Error: execve failed");
 }
 
 void	child_process_1(t_pipe_args *args, int pipe_fd[2])
@@ -31,10 +31,10 @@ void	child_process_1(t_pipe_args *args, int pipe_fd[2])
 	args->path_cmd = args->path_cmd1;
 	args->cmd = args->cmd1;
 	if (!args->path_cmd)
-		perror_exit("Error: wrong command\n");
+		perror_exit("Error: wrong command");
 	infile = open(args->file1, O_RDONLY);
 	if (infile == -1)
-		perror_exit("Error: cannot open file1\n");
+		perror_exit("Error: cannot open file1");
 	close(pipe_fd[0]);
 	execute_command(infile, pipe_fd[1], args);
 }
@@ -46,10 +46,20 @@ void	child_process_2(t_pipe_args *args, int pipe_fd[2])
 	args->path_cmd = args->path_cmd2;
 	args->cmd = args->cmd2;
 	if (!args->path_cmd)
-		perror_exit("Error: wrong command\n");
+	{
+		write(STDERR_FILENO, args->cmd[0], ft_strlen(args->cmd[0]));
+		write(STDERR_FILENO, ": command not found\n", 20);
+		exit(127);
+	}
+	//perror_exit("Error: wrong command");
 	outfile = open(args->file2, O_CREAT | O_WRONLY | O_TRUNC, 0644);
 	if (outfile == -1)
-		perror_exit("Error: cannot write in file2\n");
+	{
+		write(STDERR_FILENO, args->file2, ft_strlen(args->file2));
+   		perror(" ");
+    		exit(EXIT_FAILURE);
+	}
+	//perror_exit("Error: cannot write in file2");
 	close(pipe_fd[1]);
 	execute_command(pipe_fd[0], outfile, args);
 }
@@ -61,15 +71,15 @@ void	create_pipe(t_pipe_args *args)
 	int	pid2;
 
 	if (pipe(pipe_fd) == -1)
-		perror_exit("Error: Pipe creation failed\n");
+		perror_exit("Error: Pipe creation failed");
 	pid1 = fork();
 	if (pid1 == -1)
-		perror_exit("Error: Fork creation failed\n");
+		perror_exit("Error: Fork creation failed");
 	if (pid1 == 0)
 		child_process_1(args, pipe_fd);
 	pid2 = fork();
 	if (pid2 == -1)
-		perror_exit("Error: Fork creation failed\n");
+		perror_exit("Error: Fork creation failed");
 	if (pid2 == 0)
 		child_process_2(args, pipe_fd);
 	close(pipe_fd[0]);
