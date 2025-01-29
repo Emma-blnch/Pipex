d40b5e885da5a583f912905bba167eb106902109
Emma Blanchard <eblancha@paul-f5Ar3s2.clusters.42paris.fr>
2025-01-29 08:05:45 +0100

    modified error message for number of argv

--- srcs/ft_pipex.c
+++ srcs/ft_pipex.c
@@ -6,7 +6,7 @@
 /*   By: eblancha <eblancha@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 10:08:02 by eblancha          #+#    #+#             */
-/*   Updated: 2025/01/27 08:56:05 by eblancha         ###   ########.fr       */
+/*   Updated: 2025/01/29 08:04:13 by eblancha         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int argc, char **argv, char **envp)
 	t_pipe_args	args;
 
 	if (argc < 5)
-		return (perror_return("Usage: ./pipex file1 cmd1 cmd2 ... cmdx file2", 1));
+		return (perror_return("Usage: ./pipex file1 cmd1 cmd2 file2", 1));
 	if (access(argv[1], R_OK) == -1)
 		return (perror_return(argv[1], 1));
 	init_args(&args, argv, envp);
